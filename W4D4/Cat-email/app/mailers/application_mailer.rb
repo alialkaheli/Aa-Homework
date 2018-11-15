ba53d55f5faf2e87892d097a77e73e44b79a1923
ali alkaheli <alialkaheli@alis-mbp.attlocal.net>
2018-11-14 22:46:36 -0800

    email notification

---
+++ W4D4/Cat-email/app/mailers/application_mailer.rb
@@ -0,0 +1,8 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: 'everybody@appacademy.io'
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: user.email, subject: 'Welcome to My Awesome Site')
+  end
+end
